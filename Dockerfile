d49e551abc8fb52ed929a15229f83c33a2ac459b
Hugo van der Geest <55272066+HugovanderGeest@users.noreply.github.com>
2024-09-23 16:12:25 +0200

    Co-authored-by: teunvanderploeg <teunvanderploeg@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,23 @@
 # Base image
-FROM python:3.9-slim
+FROM python:3.12-slim
 
 # Set the working directory in the container
 WORKDIR /app
 
 # Copy requirements.txt
-COPY requirements.txt .
+COPY requirements.txt /app/
 
 # Install the required packages
 RUN pip install --no-cache-dir -r requirements.txt
-
 # Install gunicorn if it's not in requirements.txt
 RUN pip install gunicorn
 
 # Copy the rest of the application files
-COPY . .
+COPY . /app/
 
 # Expose the port that gunicorn will listen on
 EXPOSE 5000
 
 # Command to run the Flask app using gunicorn
-CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "app:app"]
+# CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "app:app"]
+CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import csv
 from flask import Flask, render_template, url_for, request, redirect
-import mysql.connector
+from flask_mysql_connector import MySQL
 from flask import render_template
 
 app = Flask(__name__)
 
+# MySQL configurations
+app.config['MYSQL_HOST'] = "oege.ie.hva.nl"
+app.config['MYSQL_USER'] = "geesths"
+app.config['MYSQL_PASSWORD'] = "NxXY6UbR$/D2nC4P"
+app.config['MYSQL_DATABASE'] = "zgeesths"
+
+mysql = MySQL(app)
 
 @app.route('/')
 def homepage():
@@ -33,44 +40,56 @@ def write_to_csv(data):
         writer.writerow([email, subject, message])
 
 
-@app.route('/submit_form', methods=['GET', 'POST'])
-def submit_form():
-    if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_csv(data)
-        return redirect('/thankyou.html')
-    else:
-        return 'Something went wrong!'
+# @app.route('/submit_form', methods=['GET', 'POST'])
+# def submit_form():
+#     if request.method == 'POST':
+#         data = request.form.to_dict()
+#         write_to_csv(data)
+#         return redirect('/thankyou.html')
+#     else:
+#         return 'Something went wrong!'
     
-# Database connectie 
-conn = mysql.connector.connect(
-    user='geesths', 
-    password='NxXY6UbR$/D2nC4P', 
-    host='oege.ie.hva.nl', 
-    database='zgeesths'
-)
+# # Database connectie 
+# conn = mysql.connector.connect(
+#     user='geesths', 
+#     password='NxXY6UbR$/D2nC4P', 
+#     host='oege.ie.hva.nl', 
+#     database='zgeesths'
+# )
+
 
 @app.route('/intressen')
-def intressen():
-    try:
-        connection = mysql.connector.connect(
-            host="oege.ie.hva.nl",
-            user="geesths",
-            password="NxXY6UbR$/D2nC4P",
-            database="zgeesths"
-        )
-        cursor = connection.cursor()
-
-        # Voert de query uit om alle gegevens uit de tabel personal_interests op te halen
-        cursor.execute("SELECT * FROM personal_interests;")
-        rows = cursor.fetchall()
-
-        connection.close()
-        
-        return render_template('personal_interests.html', rows=rows)
+def personal_interests():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM personal_interests")
+    interests = cursor.fetchall()
+    cursor.close()
+    print(interests)
     
-    except mysql.connector.Error as err:
-        return f"Database connection failed. Error: {err}"
+    return render_template('personal_interests.html', interests=interests)
+
+# @app.route('/intressen')
+# def intressen():
+#     try:
+#         connection = mysql.connector.connect(
+#             host="oege.ie.hva.nl",
+#             user="geesths",
+#             password="NxXY6UbR$/D2nC4P",
+#             database="zgeesths"
+#         )
+#         cursor = connection.cursor()
+
+
+#         # Voert de query uit om alle gegevens uit de tabel personal_interests op te halen
+#         cursor.execute("SELECT * FROM personal_interests;")
+#         rows = cursor.fetchall()
+
+#         connection.close()
+        
+#         return render_template('personal_interests.html', rows=rows)
+    
+#     except mysql.connector.Error as err:
+#         return f"Database connection failed. Error: {err}"
 
 # if __name__ == '__main__':
 #     from waitress import serve
